baed2a2c1e1d0fb239e223b3a0862ca478a8b9b9
seungjin <seungjin429@naver.com>
2019-10-11 17:11:19 +0900

    Configuration Module 구현 중

--- GroupingApp/AppDelegate.swift
+++ GroupingApp/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
    
 
-    
+    let dd = Configuration()
+    print(dd.getConfigurations())
+    print(dd.accessGoogleAPIKey)
+    print(dd.scheme)
     return true
   }
   
--- GroupingApp/Core/Configurtions/Configuration.swift
+++ GroupingApp/Core/Configurtions/Configuration.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import UIKit
+
+struct Configuration {
+
+  private enum Keys: String {
+    case configuration = "google-api-key"
+  }
+
+  var scheme: String? {
+      return Bundle.main.infoDictionary?[ApplicationConfiguration] as? String
+  }
+
+  var accessGoogleAPIKey: String? {
+    guard let apiKey = getConfigurations()?[Keys.configuration.rawValue] as? String else {
+      return nil
+    }
+
+    return apiKey
+  }
+
+  func getConfigurations() -> [String : Any]? {
+    let fileName = "Configuration"
+    let fileExtension = "plist"
+    guard let filePath = Bundle.main.path(forResource: fileName, ofType: fileExtension),
+      let configurations = NSDictionary(contentsOfFile: filePath) as? [String : Any] else {
+        return [:]
+    }
+
+    return configurations
+  }
+}
