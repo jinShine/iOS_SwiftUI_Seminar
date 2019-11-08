4eda15a5d70f7b8befe7b7171dab4c7c376023b8
seungjin <seungjin429@naver.com>
2019-11-08 12:15:05 +0900

    iOS13 이전 Window를 찾지 못하는 이슈 수정

--- GroupingApp/AppDelegate.swift
+++ GroupingApp/AppDelegate.swift
@@ -12,6 +12,8 @@ import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
