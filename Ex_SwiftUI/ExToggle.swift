677caaf827a627fbe311e89a67b984ef8626fbed
jinShine <seungjin429@naver.com>
2022-01-08 23:53:10 +0900

    Toggle 예제 추가

---
+++ Ex_SwiftUI/ExToggle.swift
@@ -0,0 +1,20 @@
+//
+//  ExToggle.swift
+//  Ex_SwiftUI
+//
+//  Created by buzz on 2022/01/08.
+//
+
+import SwiftUI
+
+struct ExToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExToggle_Previews: PreviewProvider {
+    static var previews: some View {
+        ExToggle()
+    }
+}
