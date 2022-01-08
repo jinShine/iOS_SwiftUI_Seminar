10fff9f1fee965901cceb643569130ab7dbe6d2c
jinShine <seungjin429@naver.com>
2022-01-09 00:31:48 +0900

    Toggle 예제 추가

--- Ex_SwiftUI/ExToggle.swift
+++ Ex_SwiftUI/ExToggle.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ExToggle: View {
+
+  @State private var isOn = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack {
+          //      Toggle("스위치 \(isOn.description)", isOn: $isOn) // Binding은 $를 사용해야 한다.
+        MyToggle(isOn: $isOn)
+
+        Toggle(isOn: $isOn) {
+          HStack {
+            Spacer()
+            Text("텍스트 부분")
+              .padding()
+              .foregroundColor(.brown)
+              .font(.headline)
+          }
+        }
+      }
+
     }
+}
+
+// 커스텀 해보기
+
+struct MyToggle: View {
+
+  @Binding var isOn: Bool
+
+  var body: some View {
+    Toggle("MyToggle \(isOn.description)", isOn: $isOn)
+  }
 }
 
 struct ExToggle_Previews: PreviewProvider {
