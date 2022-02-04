71d393b9cdc93b72daf5459a34accaaf0a5fb0ce
jinShine <seungjin429@naver.com>
2022-02-04 23:57:52 +0900

    EnvironmentObject 예제 추가

---
+++ Ex_SwiftUI/Sources/ExEnvironmentObject.swift
@@ -0,0 +1,20 @@
+//
+//  ExEnvironmentObject.swift
+//  Ex_SwiftUI
+//
+//  Created by buzz on 2022/02/04.
+//
+
+import SwiftUI
+
+struct ExEnvironmentObject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExEnvironmentObject_Previews: PreviewProvider {
+    static var previews: some View {
+        ExEnvironmentObject()
+    }
+}
